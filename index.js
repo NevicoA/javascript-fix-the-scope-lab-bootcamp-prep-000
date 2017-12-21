d31288bec890b66a23bf5aae29aab8a0931410c3
a.yalyshev@gmail.com YALYSHEV <a.yalyshev@gmail.com>
2017-12-21 00:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ function add2(n) {
 }
 
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function() {  
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
